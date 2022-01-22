2e11c8e44b10a3c8baf15d0bf2e70562efe0073b
C0821G1-DangHuuDu <huud90@gmail.com>
2022-01-22 14:01:15 +0700

    up git

---
+++ s02_typescript/bai_tap/bai-tap-tong-fibonacci/src/main.ts
@@ -0,0 +1,24 @@
+function fibonacci(n : number) {
+    let a = 0;
+    let b = 1;
+    let total = a+b;
+    let sum = 0;
+    for (let i = 1; i <= n; i++){
+        if (i==1){
+            console.log(a);
+            sum+=a;
+        } else if (i==2){
+            console.log(b);
+            sum+=b;
+        } else {
+            console.log(total);
+            sum+=total;
+            a=b;
+            b=total;
+            total=a+b;
+        }
+    }
+    console.log("sum of " + n +" fibonacci: "+ sum);
+}
+
+fibonacci(10);
